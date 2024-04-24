50edb635f67baf15f8a1c8e5a47fe160c03e7051
sdh20282 <sdh20282@gmail.com>
2024-04-24 17:43:28 +0900

    Feat : Implement PRODUCTS - #10.0 prisma schema

--- components/social-login.tsx
+++ components/social-login.tsx
@@ -12,10 +12,10 @@ export default function SocialLogin() {
             </svg>
           <span>Continue with Github</span>
         </Link>
-        <Link href="/sms" className="primary-btn flex h-10 items-center justify-center gap-3">
+        {/* <Link href="/sms" className="primary-btn flex h-10 items-center justify-center gap-3">
           <span><ChatBubbleOvalLeftEllipsisIcon className="size-6" /></span>
           <span>Continue with SMS</span>
-        </Link>
+        </Link> */}
       </div>
     </>
   )
