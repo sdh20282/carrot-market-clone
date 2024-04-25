5975ff98543ccbbdac89df4dfb7b6b974690d3ba
sdh20282 <sdh20282@gmail.com>
2024-04-25 18:53:30 +0900

    Feat : Implememt PRODUCTS - #10.7 Pagination Actions

--- components/list-product.tsx
+++ components/list-product.tsx
@@ -15,7 +15,7 @@ export default function ListProduct({ title, price, created_at, photo, id }: Lis
   return (
     <Link href={`/products/${id}`} className="flex gap-5">
       <div className="relative size-28 rounded-md overflow-hidden">
-        <Image src={photo} alt={title} fill />
+        <Image src={photo} alt={title} fill className="object-cover" />
       </div>
       <div className="flex flex-col gap-1 *:text-white">
         <span className="text-lg">{title}</span>
