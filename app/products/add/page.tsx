221faf61d08d337a06a15767c5a7040f35a0d93a
sdh20282 <sdh20282@gmail.com>
2024-04-25 22:35:36 +0900

    Feat : PRODUCT UPLOAD - #11.1 Form Action

---
+++ app/products/add/page.tsx
@@ -0,0 +1,79 @@
+"use client";
+
+import { useState } from "react";
+
+import Button from "@/components/button";
+import Input from "@/components/input";
+
+import { PhotoIcon } from "@heroicons/react/24/solid";
+import { uploadProduct } from "./actions";
+
+export default function AddProduct() {
+  const [preview, setPreview] = useState("");
+  const onImageChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const { target: { files } } = event;
+
+    if (!files) {
+      return;
+    }
+
+    const file = files[0];
+
+    if (!file.type.startsWith("image")) {
+      alert("이미지 파일만 업로드할 수 있습니다!");
+
+      return;
+    }
+
+    if (file.size > 1000000) {
+      alert("이미지 파일의 용량이 너무 큽니다!!");
+
+      return;
+    }
+
+    const url = URL.createObjectURL(file);
+    setPreview(url);
+  };
+
+  return (
+    <div>
+      <form action={uploadProduct} className="p-5 flex flex-col gap-5">
+        <label 
+          htmlFor="photo" 
+          className="border-2 aspect-square flex items-center justify-center flex-col text-neutral-300 border-neutral-300 rounded-md border-dashed cursor-pointer bg-center bg-cover bg-no-repeat" 
+          style={{
+            backgroundImage: `url(${preview})`,
+          }}>
+            {
+              preview
+              ? null
+              : <>
+                <PhotoIcon className="w-20" />
+                <div className="text-neutral-400 text-sm">사진을 추가해주세요.</div>
+              </>
+            }
+        </label>
+        <input onChange={onImageChange} type="file" id="photo" name="photo" hidden />
+        <Input
+          name="title"
+          type="text"
+          placeholder="제목"
+          required
+        />
+        <Input
+          name="price"
+          type="number"
+          placeholder="가격"
+          required
+        />
+        <Input
+          name="description"
+          type="text"
+          placeholder="상세 설명"
+          required
+        />
+        <Button text="작성 완료" />
+      </form>
+    </div>
+  )
+}
