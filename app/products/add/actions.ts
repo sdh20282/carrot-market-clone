f3052c672a943b7d71cb1a12ab7dc43c17b87f52
sdh20282 <sdh20282@gmail.com>
2024-04-25 23:00:04 +0900

    Feat : Implement PRODUCT UPLOAD - #11.2 Product Upload

--- app/products/add/actions.ts
+++ app/products/add/actions.ts
@@ -1,6 +1,30 @@
 "use server";
 
-export async function uploadProduct(formData: FormData) {
+import { notFound, redirect } from "next/navigation";
+import { File } from "buffer";
+import fs from "fs/promises";
+
+import { z } from "zod";
+
+import db from "@/lib/db";
+import getSession from "@/lib/session/get-session";
+
+const productSchema = z.object({
+  photo: z.string({
+    required_error: "Photo is required!",
+  }),
+  title: z.string({
+    required_error: "Title is required!",
+  }),
+  description: z.string({
+    required_error: "Description is required!",
+  }),
+  price: z.coerce.number({
+    required_error: "Price is required!",
+  }),
+})
+
+export async function uploadProduct(prev: any, formData: FormData) {
   const data = {
     photo: formData.get("photo"),
     title: formData.get("title"),
@@ -8,6 +32,40 @@ export async function uploadProduct(formData: FormData) {
     description: formData.get("description"),
   }
 
-  console.log(data);
-  
+  if (data.photo instanceof File) {
+    const photoData = await data.photo.arrayBuffer();
+    await fs.appendFile(`./public/${data.photo.name}`, Buffer.from(photoData));
+    data.photo = `/${data.photo.name}`;
+  }
+
+  const result = productSchema.safeParse(data);
+
+  if (!result.success) {
+    return result.error.flatten();
+  }
+
+  const session = await getSession();
+
+  if (!session.id) {
+    return notFound();
+  }
+
+  const product = await db.product.create({
+    data: {
+      photo: result.data.photo,
+      title: result.data.title,
+      price: result.data.price,
+      description: result.data.description,
+      user: {
+        connect: {
+          id: session.id,
+        }
+      }
+    },
+    select: {
+      id: true,
+    }
+  });
+
+  redirect(`/products/${product.id}`);
 }
--- app/products/add/page.tsx
+++ app/products/add/page.tsx
@@ -7,6 +7,7 @@ import Input from "@/components/input";
 
 import { PhotoIcon } from "@heroicons/react/24/solid";
 import { uploadProduct } from "./actions";
+import { useFormState } from "react-dom";
 
 export default function AddProduct() {
   const [preview, setPreview] = useState("");
@@ -35,9 +36,11 @@ export default function AddProduct() {
     setPreview(url);
   };
 
+  const [state, dispatch] = useFormState(uploadProduct, null);
+
   return (
     <div>
-      <form action={uploadProduct} className="p-5 flex flex-col gap-5">
+      <form action={dispatch} className="p-5 flex flex-col gap-5">
         <label 
           htmlFor="photo" 
           className="border-2 aspect-square flex items-center justify-center flex-col text-neutral-300 border-neutral-300 rounded-md border-dashed cursor-pointer bg-center bg-cover bg-no-repeat" 
@@ -50,6 +53,7 @@ export default function AddProduct() {
               : <>
                 <PhotoIcon className="w-20" />
                 <div className="text-neutral-400 text-sm">사진을 추가해주세요.</div>
+                {state?.fieldErrors.photo}
               </>
             }
         </label>
@@ -59,18 +63,21 @@ export default function AddProduct() {
           type="text"
           placeholder="제목"
           required
+          errors={state?.fieldErrors.title}
         />
         <Input
           name="price"
           type="number"
           placeholder="가격"
           required
+          errors={state?.fieldErrors.price}
         />
         <Input
           name="description"
           type="text"
           placeholder="상세 설명"
           required
+          errors={state?.fieldErrors.description}
         />
         <Button text="작성 완료" />
       </form>
