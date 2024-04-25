810a770835188bf2007238237d7efff571b293a8
sdh20282 <sdh20282@gmail.com>
2024-04-26 03:58:10 +0900

    Refactor : remove unused code

--- components/input.tsx
+++ components/input.tsx
@@ -23,24 +23,3 @@ export default function Input({
     </div>
   )
 }
-
-// const _Input = ({
-//   name,
-//   errors = [],
-//   ...rest
-// }: InputProps & InputHTMLAttributes<HTMLInputElement>) => {
-//   return (
-//     <div className="flex flex-col gap-2">
-//       <input
-//         name={name}
-//         className="bg-transparent rounded-md w-full h-10 focus:outline-none ring-2 focus:ring-4 ring-neutral-200 transition focus:ring-orange-500 border-none placeholder:text-neutral-400"
-//         {...rest}
-//       />
-//       {
-//         errors.map((error, idx) => <span key={idx} className="text-red-500 font-medium">{error}</span>)
-//       }
-//     </div>
-//   )
-// }
-
-// export default forwardRef(_Input);
