d7eac7e77064284522acb8c5fbb2d912406be5c2
sdh20282 <sdh20282@gmail.com>
2024-04-30 23:52:12 +0900

    Feat : Implement CACHING - #13.3 revalidatePath

--- app/(tabs)/home/page.tsx
+++ app/(tabs)/home/page.tsx
@@ -1,5 +1,5 @@
 import Link from "next/link";
-import { unstable_cache as nextCache } from "next/cache";
+import { unstable_cache as nextCache, revalidatePath } from "next/cache";
 
 import ProductList from "@/components/product-list";
 import db from "@/lib/db";
@@ -9,12 +9,12 @@ import { Prisma } from "@prisma/client";
 const getCachedProducts = nextCache(
   getInitialProducts,
   ['home-products'],
-  {
-    revalidate: 60,
-  }
 );
 
 async function getInitialProducts() {
+  console.log('hit!!');
+  
+
   const products = await db.product.findMany({
     select: {
       title: true,
@@ -40,10 +40,18 @@ export const metadata = {
 
 export default async function Products() {
   const initialProducts = await getCachedProducts();
+  const revalidate = async () => {
+    "use server";
+
+    revalidatePath("/home");
+  }
 
   return (
     <div>
       <ProductList initialProducts={initialProducts} />
+      <form action={revalidate}>
+        <button>Revalidate</button>
+      </form>
       <Link href="/products/add" className="bg-orange-500 flex items-center justify-center rounded-full size-16 fixed bottom-24 right-8 text-white transition-colors hover:bg-orange-400">
         <PlusIcon className="size-10" />
       </Link>
