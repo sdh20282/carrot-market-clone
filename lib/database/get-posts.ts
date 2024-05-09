a1390fdfae1569fb68182462d9c133947e46a551
sdh20282 <sdh20282@gmail.com>
2024-05-09 16:03:32 +0900

    Feat : Impelment OPTIMISTIC UPDATES - #14.1 See Posts

---
+++ lib/database/get-posts.ts
@@ -0,0 +1,21 @@
+import db from "../db";
+
+export async function getPosts() {
+  const posts = await db.post.findMany({
+    select: {
+      id: true,
+      title: true,
+      description: true,
+      views: true,
+      created_at: true,
+      _count: {
+        select: {
+          comments: true,
+          likes: true
+        }
+      }
+    }
+  });
+
+  return posts;
+}
